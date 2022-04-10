25141fec8682ab79db7f144f501e4479271f84ec
novakvova <novakvova@gmail.com>
2022-04-10 17:03:33 +0300

    Select image is good

--- Android/app/src/main/java/com/example/shop/MainActivity.java
+++ Android/app/src/main/java/com/example/shop/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.shop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,6 +22,9 @@ import com.example.shop.dto.ValidationCreateProductDTO;
 import com.example.shop.network.ProductService;
 import com.google.gson.Gson;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -28,7 +35,11 @@ public class MainActivity extends AppCompatActivity {
     EditText editTextName;
     EditText editTextPrice;
     EditText editTextDescription;
-    EditText editTextImage;
+
+    // constant to compare
+    // the activity result code
+    int SELECT_PICTURE = 200;
+    String sImage="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +49,53 @@ public class MainActivity extends AppCompatActivity {
         editTextName = findViewById(R.id.editTextName);
         editTextPrice = findViewById(R.id.editTextPrice);
         editTextDescription = findViewById(R.id.editTextDescription);
-        editTextImage = findViewById(R.id.editTextImage);
     }
+
+    public void handleSelectImageClick(View view) {
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+
+    // this function is triggered when user
+    // selects the image from the imageChooser
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                Uri uri = data.getData();
+                // update the preview image in the layout
+                //IVPreviewImage.setImageURI(uri);
+                Bitmap bitmap= null;
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                // initialize byte stream
+                ByteArrayOutputStream stream=new ByteArrayOutputStream();
+                // compress Bitmap
+                bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
+                // Initialize byte array
+                byte[] bytes=stream.toByteArray();
+                // get base64 encoded string
+                sImage= Base64.encodeToString(bytes,Base64.DEFAULT);
+            }
+        }
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -73,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
                 editTextName.getText().toString(),
                 editTextPrice.getText().toString(),
                 editTextDescription.getText().toString(),
-                editTextImage.getText().toString()
+                sImage
         );
         ProductService
                 .getInstance()
@@ -84,6 +140,8 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(Call<CreateProductResultDTO> call, Response<CreateProductResultDTO> response) {
                         if(response.isSuccessful()) {
                             CreateProductResultDTO result = response.body();
+                            Intent intent = new Intent(MainActivity.this, ProductsActivity.class);
+                            startActivity(intent);
                         }
                         else {
                             try {
